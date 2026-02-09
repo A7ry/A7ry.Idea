3bc6943f08cdc1b2e817a00c61525e62f8940595
A7ry <avrypower100@gmail.com>
2026-02-09 08:28:51 +0400

    Update vault.js

--- vault.js
+++ vault.js
@@ -1,10 +1,24 @@
-const vault = {
-  "Games": [
-    "vault/games/idea1.txt",
-    "vault/games/idea2.txt"
-  ],
-
-  "Thoughts": [
-    "vault/thoughts/future.txt"
-  ]
-};
+const vault = [
+  {
+    name: "Games",
+    icon: "🎮",
+    files: [
+      "vault/games/idea1.txt",
+      "vault/games/idea2.txt"
+    ]
+  },
+  {
+    name: "Arduino Projects",
+    icon: "⚙️",
+    files: [
+      "vault/arduino/robotcar.txt"
+    ]
+  },
+  {
+    name: "Thoughts",
+    icon: "💡",
+    files: [
+      "vault/thoughts/future.txt"
+    ]
+  }
+];
